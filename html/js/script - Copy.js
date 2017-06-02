7040ad678dec5b7651d1b876de7ce926d26c1ac3
MasterlyMaids <masterlymaids@gmail.com>
2017-06-02 11:30:44 -0400

    Initial Launch

--- html/js/script - Copy.js
+++ html/js/script - Copy.js
@@ -1,4 +1,4 @@
-﻿  var br1 = $('input[name="checkboxbr1Input"]');
+  var br1 = $('input[name="checkboxbr1Input"]');
   var br2 = $('input[name="checkboxbr2Input"]');
   var br3 = $('input[name="checkboxbr3Input"]');
   var br4 = $('input[name="checkboxbr4Input"]');
@@ -61,63 +61,100 @@
   f4.change(function(){f1.prop('checked',false);});
   f4.change(function(){f2.prop('checked',false);});
   f4.change(function(){f3.prop('checked',false);});  
+ 
   
   $('div.mcheckbox').click(function(){
+    var bafix = document.querySelector(".bafix")
+    var conf1 = document.querySelector('#conf1');
     var check1 = document.getElementById('check1');
     if (document.getElementById('checkboxbr1Input').checked) {
         check1.style.display = 'inline-block';
+        conf1.style.backgroundColor = '#55575b';
+        conf1.style.border = '3px solid #7d8087';
     } 
     else if (document.getElementById('checkboxbr2Input').checked) {
         check1.style.display = 'inline-block';
+        conf1.style.backgroundColor = '#55575b';
+        conf1.style.border = '3px solid #7d8087';
     }
     else if (document.getElementById('checkboxbr3Input').checked) {
         check1.style.display = 'inline-block';
+        conf1.style.backgroundColor = '#55575b';
+        conf1.style.border = '3px solid #7d8087';
     }
     else if (document.getElementById('checkboxbr4Input').checked) {
         check1.style.display = 'inline-block';
+        conf1.style.backgroundColor = '#55575b';
+        conf1.style.border = '3px solid #7d8087';
     }
     else if (document.getElementById('checkboxbr5Input').checked) {
         check1.style.display = 'inline-block';
+        conf1.style.backgroundColor = '#55575b';
+        conf1.style.border = '3px solid #7d8087';
     }
     else {
       check1.style.display = 'none';
+      conf1.style.backgroundColor = '#28292b';
+      conf1.style.border = '3px solid #35373a';
     }
   });
 
   $('div.mbacheckbox').click(function(){
+    var conf2 = document.querySelector('#conf2');
     var check2 = document.getElementById('check2');
     if (document.getElementById('checkboxba1Input').checked) {
         check2.style.display = 'inline-block';
+        conf2.style.backgroundColor = '#55575b';
+        conf2.style.border = '3px solid #7d8087';
     } 
     else if (document.getElementById('checkboxba2Input').checked) {
         check2.style.display = 'inline-block';
+        conf2.style.backgroundColor = '#55575b';
+        conf2.style.border = '3px solid #7d8087';
     }
     else if (document.getElementById('checkboxba3Input').checked) {
         check2.style.display = 'inline-block';
+        conf2.style.backgroundColor = '#55575b';
+        conf2.style.border = '3px solid #7d8087';
     }
     else if (document.getElementById('checkboxba4Input').checked) {
         check2.style.display = 'inline-block';
+        conf2.style.backgroundColor = '#55575b';
+        conf2.style.border = '3px solid #7d8087';
     }
     else {
       check2.style.display = 'none';
+      conf2.style.backgroundColor = '#28292b';
+      conf2.style.border = '3px solid #35373a';
     }
   });
 
   $('div.mfcheckbox').click(function(){
+    var conf3 = document.querySelector('#conf3');
     var check3 = document.getElementById('check3');
     if (document.getElementById('checkboxf1Input').checked) {
         check3.style.display = 'inline-block';
+        conf3.style.backgroundColor = '#55575b';
+        conf3.style.border = '3px solid #7d8087';
     } 
     else if (document.getElementById('checkboxf2Input').checked) {
         check3.style.display = 'inline-block';
+        conf3.style.backgroundColor = '#55575b';
+        conf3.style.border = '3px solid #7d8087';
     }
     else if (document.getElementById('checkboxf3Input').checked) {
         check3.style.display = 'inline-block';
+        conf3.style.backgroundColor = '#55575b';
+        conf3.style.border = '3px solid #7d8087';
     }
     else if (document.getElementById('checkboxf4Input').checked) {
         check3.style.display = 'inline-block';
+        conf3.style.backgroundColor = '#55575b';
+        conf3.style.border = '3px solid #7d8087';
     }
     else {
       check3.style.display = 'none';
+      conf3.style.backgroundColor = '#28292b';
+      conf3.style.border = '3px solid #35373a';
     }
   });
